68712e3ec7b18f4af96fc0afdebabd4a91d1dbc9
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-23 13:28:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const boxes = document.querySelectorAll(".box");
+
+    
+    boxes[0].classList.add('selected');
+    boxes[0].querySelector('.radio-button').checked = true;
+
+    boxes.forEach(box => {
+        box.addEventListener("click", function () {
+            const radioButton = this.querySelector('.radio-button');
+
+            
+            const radioButtons = document.querySelectorAll('input[name="pairSelection"]');
+            radioButtons.forEach(radio => {
+                if (radio !== radioButton) {
+                    radio.checked = false;
+                }
+            });
+
+            
+            const selectedBox = document.querySelector('.selected');
+            if (selectedBox && selectedBox !== this) {
+                selectedBox.classList.remove('selected');
+                selectedBox.querySelector('.content-section').style.display = 'none';
+            }
+            this.classList.add('selected');
+            radioButton.checked = !radioButton.checked;
+
+            const contentSection = this.querySelector('.content-section');
+            if (contentSection) {
+                contentSection.style.display = radioButton.checked ? 'block' : 'none';
+            }
+        });
+
+        box.addEventListener("mouseenter", function () {
+            if (!this.classList.contains('selected')) {
+                this.classList.add('hover');
+            }
+        });
+
+        box.addEventListener("mouseleave", function () {
+            this.classList.remove('hover');
+        });
+
+        
+        const dropdownButton = box.querySelector('.dropdown select');
+        if (dropdownButton) {
+            dropdownButton.addEventListener("click", function (event) {
+                box.click();
+                event.stopPropagation(); 
+            });
+        }
+    });
+});
